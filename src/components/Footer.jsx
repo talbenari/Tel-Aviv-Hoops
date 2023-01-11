9bd9af94ae4cbc19b05b92aa7e38a9465230ab0d
Tal Ben ari <talbenari@Tals-MacBook-Air.local>
2023-01-11 09:44:21 +0200

    before merging with master

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,6 +9,8 @@ import './footer.css';
 
 function Footer() {
 
+
+
     const theme = createTheme({
         palette: {
             primary: {
@@ -24,12 +26,17 @@ function Footer() {
         <ThemeProvider theme={theme}>
             <AppBar position='static' style={{ bottom: 0, height: 500, marginTop: 600 }}>
                 <Toolbar style={{ height: '100%', justifyContent: 'space-evenly', alignItems: 'center', flexDirection: 'column' }}>
+                    {/* <iframe src="https://storage.googleapis.com/maps-solutions-s3j131omlq/locator-plus/j6l6/locator-plus.html"
+                        width="100%" height="100%"
+                        style={{border:0}}
+                        loading="lazy">
+                    </iframe> */}
                     <Stack direction="row" spacing={8}>
                         <Button variant='' style={{ borderRadius: 20 }}><a target='blank' href='https://www.instagram.com/accounts/login/' color='white'><InstagramIcon /></a></Button>
                         <Button variant='' style={{ borderRadius: 20 }}><a target='blank' href='https://www.facebook.com/'><FacebookIcon /></a></Button>
                         <Button variant='' style={{ borderRadius: 20 }}><a target='blank' href='https://twitter.com/i/flow/login'><TwitterIcon /></a></Button>
                     </Stack>
-                    <Stack alignItems='center'>
+                    <Stack alignItems='center' textAlign='center'>
                         <Typography variant='h4'>
                             Sign up and play with Tel-Aviv hoops
                         </Typography>
@@ -37,7 +44,7 @@ function Footer() {
                     </Stack>
                     <Stack>
                         <Typography variant='caption'>
-                            © 2023 TelAvivHoops
+                            © 2023 TLVHoops
                         </Typography>
                     </Stack>
                 </Toolbar>
