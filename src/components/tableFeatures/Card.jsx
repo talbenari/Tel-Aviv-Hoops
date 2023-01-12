a4c31d326f13f828178072d6e354f46b829a2de1
talbenari <talbenari865@gmail.com>
2023-01-12 16:17:36 +0200

    table has buttons

---
+++ src/components/tableFeatures/Card.jsx
@@ -0,0 +1,15 @@
+import Map from './Map.jsx';
+import './infoCard.css';
+import { Card } from '@mui/material';
+
+function InfoCard() {
+
+
+    return (
+        <Card>
+            <Map />
+        </Card>
+    )
+}
+
+export default InfoCard;
