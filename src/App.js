014e237e251ece99f6b79ea238d01058a705045f
roygutter <royg910@gmail.com>
2023-01-12 14:31:21 +0200

    haha

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
 import React from "react";
-import Footer from './components/Footer';
-import NavBar from './components/NavBar';
-import GetGamesData from "./components/tableFeatures/GetGamesData"
-
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import Info from "./components/Info";
+import FullSchedule from "./components/schedule/FullSchedule";
+import GetGamesData from "./components/schedule/GetGamesData";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <GetGamesData/>
+      <br />
+
+      <GetGamesData />
+      <br />
       <Footer />
+     
     </div>
   );
 }
 
 export default App;
-
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import "../components/RulesDisplay"
 import RulesDisplay from "../components/RulesDisplay"
 import TlvRun from "../components/TlvRun"
+import Info from "../components/Info"
 function Home() {
     return (
         <div>
             <TlvRun></TlvRun>
+            <Info />
             <RulesDisplay></RulesDisplay>
         </div>
     )
