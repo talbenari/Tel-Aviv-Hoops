1794bf85879ce3f1e7bcaff4765d02b0bf096453
roygutter <royg910@gmail.com>
2023-01-15 09:47:37 +0200

    styles

--- src/components/tableFeatures/Schedule.jsx
+++ src/components/tableFeatures/Schedule.jsx
@@ -1,9 +1,97 @@
+
+
+// import * as React from 'react';
+// import { DataGrid } from '@mui/x-data-grid';
+// import { CacheProvider } from '@emotion/react';
+// import createCache from '@emotion/cache';
+// import { type } from '@testing-library/user-event/dist/type';
+// import { ThemeProvider, createTheme, Typography, Button } from '@mui/material';
+// import styles from './Schedule.css'
+
+
+
+
+
+
+
+// const cache = createCache({
+//     key: 'css',
+//     prepend: true,
+// });
+
+
+// function Schedule(props) {
+
+
+//     const columns = [
+//         { field: 'date', headerName: 'Day', width: 250, headerClassName: 'title', cellClassName: 'cells' },
+//         { field: 'court', headerName: 'Court', width: 250, headerClassName: 'title', cellClassName: 'cells' },
+//         { field: 'time', headerName: 'Time', width: 250, headerClassName: 'title', cellClassName: 'cells' },
+//         {
+//             field: 'playersSign',
+//             headerName: 'playersSign',
+//             width: 250, headerClassName: 'title', cellClassName: 'cells'
+//         },
+//         {
+
+//         }
+
+//     ];
+
+
+//     const rows = props.games.map((game, index) => (
+//         {
+//             id: index,
+//             date: game.date,
+//             court: game.court,
+//             time: `${game.startTime}-${game.endTime}`,
+//             playersSign: `${game.playersSign}/15`,
+
+//         }
+//     ))
+//     const newRows = rows.slice(5);
+
+//     const theme = createTheme({
+//         typography: {
+
+//             fontSize: 20,
+//         },
+//     });
+//     return (
+//         <ThemeProvider theme={theme}>
+//             <Typography>
+//                 <CacheProvider value={cache}>
+
+
+//                     <div className='table' style={{ height: 500, width: '90vw' }}>
+//                         <DataGrid
+//                             className='table'
+//                             rows={newRows}
+//                             columns={columns}
+//                             pageSize={5}
+//                             rowsPerPageOptions={[5]}
+//                             rowHeight={70}
+
+
+//                         />
+//                     </div>
+//                 </CacheProvider>
+//             </Typography>
+//         </ThemeProvider>
+//     );
+// };
+
+// export default Schedule;
+
+
+
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { Button, Typography } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { NavLink } from 'react-router-dom';
 import InfoCard from './InfoCard';
+import styles from './Schedule.css'
 
 
 function Schedule(props) {
@@ -23,16 +111,16 @@ function Schedule(props) {
     });
 
     const columns = [
-        { field: 'date', headerName: 'Date', width: 150 },
-        { field: 'court', headerName: 'Court', width: 130 },
-        { field: 'time', headerName: 'Time', width: 130 },
-        { field: 'playersSign', headerName: 'playersSign', width: 200 },
+        { field: 'date', headerName: 'Date', width: 200, headerClassName: 'title', cellClassName: 'cells' },
+        { field: 'court', headerName: 'Court', width: 200, headerClassName: 'title', cellClassName: 'cells' },
+        { field: 'time', headerName: 'Time', width: 200, headerClassName: 'title', cellClassName: 'cells' },
+        { field: 'playersSign', headerName: 'playersSign', width: 200, headerClassName: 'title', cellClassName: 'cells' },
         {
-            field: '', width: 150, renderCell: ({ row }) => {
+            field: '', width: 150, headerClassName: 'title', cellClassName: 'cells', renderCell: ({ row }) => {
                 if (row.playersSign === '15/15') {
                     return <ThemeProvider theme={theme}>
-                        <NavLink to='/card' style={{textDecoration: 'none'}}>
-                            <Button variant='contained' color='secondary' style={{width: theme.xs.width}}>
+                        <NavLink to='/card' style={{ textDecoration: 'none' }}>
+                            <Button variant='contained' color='secondary' style={{ width: theme.xs.width }}>
                                 <Typography variant='caption'>
                                     Join Wait-list
                                 </Typography>
@@ -41,19 +129,19 @@ function Schedule(props) {
                     </ThemeProvider>
                 } else {
                     return <ThemeProvider theme={theme}>
-                    {/* <NavLink to='/card' style={{textDecoration: 'none'}}> */}
-                        <Button variant='contained' style={{width: theme.xs.width}} onClick={() => {
-                            return <div style={{position: 'absolute', alignSelf: 'center', justifySelf: 'center'}}>
-                                    <InfoCard/>
-                                </div>
-                            
+                        {/* <NavLink to='/card' style={{textDecoration: 'none'}}> */}
+                        <Button variant='contained' style={{ width: theme.xs.width }} onClick={() => {
+                            return <div style={{ position: 'absolute', alignSelf: 'center', justifySelf: 'center' }}>
+                                <InfoCard />
+                            </div>
+
                         }}>
                             <Typography variant='caption'>
                                 Join Game
                             </Typography>
                         </Button>
-                    {/* </NavLink> */}
-                </ThemeProvider>
+                        {/* </NavLink> */}
+                    </ThemeProvider>
                 }
             }
         }
@@ -70,15 +158,18 @@ function Schedule(props) {
 
         }
     ))
-
+    const newRows = rows.slice(5);
 
     return (
         <div style={{ height: 400, width: '100%' }}>
             <DataGrid
-                rows={rows}
+                rows={newRows}
                 columns={columns}
-                pageSize={8}
-                rowsPerPageOptions={[5]}
+                // pageSize={5}
+                // rowsPerPageOptions={[5]}
+                hideFooterPagination
+                hideFooter
+                disableSelectionOnClick
             />
         </div>
     );
